af040c14855a7555701f85ea0e5d47e8e2045a36
Manoj Thakur <manoj.thakur@cotiviti.com>
2020-11-05 17:15:16 +0545

    poweshell script now made to read from relative path instead of absolute path

--- web-api-tests/code_coverage.ps1
+++ web-api-tests/code_coverage.ps1
@@ -3,9 +3,9 @@
     $currentDirectory = Get-Location
     $testProjectPath = "$currentDirectory\web-api-tests"
     $testSettingsPath = "$currentDirectory\web-api-tests\codecoverage.runsettings"
-    $testResultsFolder = $currentDirectory
 
 try {
+
     Write-Host "Cache clean up running..." -ForegroundColor Green
     $testResultDirectory = "$currentDirectory\TestResults"
     if ((Test-Path -Path $testResultDirectory -PathType Container)) 
@@ -18,22 +18,18 @@ try {
     Write-Host "dotnet test running..." -ForegroundColor Green
     dotnet test $testProjectPath --settings:$testSettingsPath 
     $recentCoverageFile = Get-ChildItem -File -Filter *.coverage -Path $testResultDirectory -Name -Recurse | Select-Object -First 1;
-    
-    
-    Write-Host "coverage file Location $recentCoverageFile"
-    Write-Host "full coverage file Location $testResultsFolder\$recentCoverageFile"
-    echo $recentCoverageFile
-    echo $testResultsFolder'\'$recentCoverageFile
-
-    write-host 'Test Completed'  -ForegroundColor Green
+    write-host 'test successful'  -ForegroundColor Green
 
 
-    C:\Users\i157864\.nuget\packages\microsoft.codecoverage\16.7.1\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze  /output:$testResultsFolder\TestResults\MyTestOutput.coveragexml  $testResultsFolder'\'$recentCoverageFile
+    Write-Host "converting coverage file to coverageXml..." -ForegroundColor Green
+    C:\Users\i157864\.nuget\packages\microsoft.codecoverage\16.7.1\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze  /output:$testResultDirectory\MyTestOutput.coveragexml  $testResultDirectory'\'$recentCoverageFile
     write-host 'CoverageXML Generated'  -ForegroundColor Green
 
-    dotnet C:\Users\i157864\.nuget\packages\reportgenerator\4.7.1\tools\netcoreapp2.1\ReportGenerator.dll "-reports:$testResultsFolder\TestResults\MyTestOutput.coveragexml" "-targetdir:$testResultsFolder\TestResults\coveragereport"
-    write-host 'CoverageReport Published'  -ForegroundColor Green
 
+    Write-Host "converting Html report from CoverageXML..." -ForegroundColor Green
+    dotnet C:\Users\i157864\.nuget\packages\reportgenerator\4.7.1\tools\netcoreapp2.1\ReportGenerator.dll "-reports:$testResultDirectory\MyTestOutput.coveragexml" "-targetdir:$testResultDirectory\coveragereport"
+    write-host 'CoverageReport Published'  -ForegroundColor Green
+    
 }
 catch {
 
